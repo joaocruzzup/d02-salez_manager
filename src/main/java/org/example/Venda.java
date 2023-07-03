6ae2d8f86969a60bf769bbecaabb2e153b0d2ead
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-03 16:59:58 -0300

    :sparkles: feat: adicionado classes iniciais do projeto

---
+++ src/main/java/org/example/Venda.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Venda {
+    private Cliente cliente;
+    private Vendedor vendedor;
+    private BigDecimal valor;
+    private LocalDate dataRegistro;
+
+    public Venda(Cliente cliente, Vendedor vendedor, BigDecimal valor, LocalDate dataRegistro) {
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.valor = valor;
+        this.dataRegistro = dataRegistro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(LocalDate dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+}
