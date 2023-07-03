ae49b3f16fa93dc673f7d9c29c037e27420a4c1d
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-03 17:58:29 -0300

    git commit -m :recycle: refactor: criando classe bancovendedoresclientes

--- src/main/java/org/example/BancoDeVendas.java
+++ src/main/java/org/example/BancoDeVendas.java
@@ -6,24 +6,5 @@ import java.util.List;
 import java.util.Map;
 
 public class BancoDeVendas {
-    private List<Vendedor> listaVendedores = new ArrayList<>();
-    private List<Cliente> listaClientes = new ArrayList<>();
-    private Map<String, List<Cliente>> vendedoresClientes = new HashMap<>() ;
-
-    public void cadastrarCliente(Vendedor vendedor, Cliente cliente){
-        listaClientes.add(cliente);
-        if (!vendedoresClientes.containsKey(vendedor.getCpf())){
-            // Refatorar futuramente esse código para chamar o método cadastrarVendedor
-            List<Cliente> listaClienteDoVendedor = new ArrayList<>();
-            listaClienteDoVendedor.add(cliente);
-            vendedoresClientes.put(vendedor.getCpf(), listaClienteDoVendedor);
-        }
-        vendedoresClientes.get(vendedor.getCpf()).add(cliente);
-    }
-
-    public void cadastrarVendedor(Vendedor vendedor){
-        listaVendedores.add(vendedor);
-        vendedoresClientes.put(vendedor.getCpf(), new ArrayList<>());
-    }
 
 }
---
+++ src/main/java/org/example/BancoVendedoresClientes.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BancoVendedoresClientes {
+    private List<Vendedor> listaVendedores = new ArrayList<>();
+    private List<Cliente> listaClientes = new ArrayList<>();
+    private Map<String, List<Cliente>> vendedoresClientes = new HashMap<>() ;
+
+    public void cadastrarCliente(Vendedor vendedor, Cliente cliente){
+        listaClientes.add(cliente);
+        if (!vendedoresClientes.containsKey(vendedor.getCpf())){
+            // Refatorar futuramente esse código para chamar o método cadastrarVendedor
+            List<Cliente> listaClienteDoVendedor = new ArrayList<>();
+            listaClienteDoVendedor.add(cliente);
+            vendedoresClientes.put(vendedor.getCpf(), listaClienteDoVendedor);
+        }
+        vendedoresClientes.get(vendedor.getCpf()).add(cliente);
+    }
+
+    public void cadastrarVendedor(Vendedor vendedor){
+        listaVendedores.add(vendedor);
+        vendedoresClientes.put(vendedor.getCpf(), new ArrayList<>());
+    }
+
+}
