547ea6bab037add598338814747baafea183bcbd
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-03 17:41:40 -0300

    :sparkles: feat: adicionado metodos de cadastro

--- src/main/java/org/example/Cadastro.java
+++ src/main/java/org/example/Cadastro.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.List;
 
-public interface Cadastravel {
+public interface Cadastro {
     public abstract void cadastrar(Object obj);
     public abstract void listar(List<Object> entidade);
 }
