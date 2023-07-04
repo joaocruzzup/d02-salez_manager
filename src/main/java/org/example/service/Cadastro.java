4bac60911f9d761cd277670e34cf25a2afa22047
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-04 18:47:21 -0300

    :boom: fix: corrigido validacoes

--- src/main/java/org/example/service/Cadastro.java
+++ src/main/java/org/example/service/Cadastro.java
@@ -1,10 +1,9 @@
 package org.example.service;
 
+import org.example.Exceptions.CpfJaExistenteException;
 import org.example.model.Usuario;
 
-import java.util.List;
-
 public interface Cadastro {
-    public abstract void cadastrar(Usuario usuario);
+    public abstract void cadastrar(Usuario usuario) throws CpfJaExistenteException;
     public abstract void listar();
 }
--- src/main/java/org/example/service/ValidaCliente.java
+++ src/main/java/org/example/service/ValidaCliente.java
@@ -1,7 +1,7 @@
 package org.example.service;
 
 
-import org.example.Exceptions.CpfRepetidoException;
+import org.example.Exceptions.CpfJaExistenteException;
 import org.example.Exceptions.EmailInvalidoException;
 import org.example.Exceptions.EmailRepetidoException;
 import org.example.model.Cliente;
@@ -9,5 +9,5 @@ import org.example.model.Cliente;
 public interface ValidaCliente {
     public abstract boolean validarEmailCliente(Cliente cliente) throws EmailInvalidoException;
     public abstract boolean validarEmailRepetidoCliente(Cliente cliente) throws EmailRepetidoException;
-    public abstract boolean validarCpfRepetidoCliente(Cliente cliente) throws CpfRepetidoException;
+    public abstract boolean validarCpfExisteCliente(Cliente cliente) throws CpfJaExistenteException;
 }
