96717f4bebb6700599f33b81988c9a73f39c9c23
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-04 09:49:35 -0300

    :sparkles: feat: adicionado metodos nas classes bancos

--- src/main/java/org/example/database/BancoDeVendas.java
+++ src/main/java/org/example/database/BancoDeVendas.java
@@ -4,13 +4,24 @@ import org.example.model.Cliente;
 import org.example.model.Venda;
 import org.example.model.Vendedor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BancoDeVendas {
-    private Vendedor vendedor;
-    private Venda venda;
-    private Cliente cliente;
+    BancoVendedoresClientes bancoVendedoresClientes = new BancoVendedoresClientes();
+    private List<Venda> listaVendas = new ArrayList<>();
 
     public void cadastrarVenda(Venda venda, Vendedor vendedor, Cliente cliente){
-
+        if (bancoVendedoresClientes.verificarClienteExiste(cliente.getCpf()) && bancoVendedoresClientes.verificarVendedorExiste(vendedor.getCpf())) {
+            venda.setCliente(cliente);
+            venda.setVendedor(vendedor);
+            listaVendas.add(venda);
+        } else {
+            throw new IllegalArgumentException("Erro: Vendedor ou Cliente não estão cadastrados no sistema");
+        }
     }
 
+    public List<Venda> getListaVendas() {
+        return listaVendas;
+    }
 }
