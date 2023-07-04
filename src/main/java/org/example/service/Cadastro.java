1ec02762c769a0b31d3fa9b80de74b8b9decd533
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-04 14:25:04 -0300

    :sparkles: feat: adicionado classe service e interface cadastro

--- src/main/java/org/example/service/Cadastro.java
+++ src/main/java/org/example/service/Cadastro.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface Cadastro {
     public abstract void cadastrar(Usuario usuario);
-    public abstract void listar(List<Usuario> usuario);
+    public abstract void listar();
 }
