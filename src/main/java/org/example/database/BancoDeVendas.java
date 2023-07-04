b3030fb2aae2cfa2e19f62a486a2ed04dad4b8e8
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-04 14:05:44 -0300

    :recycle: refactor: refatorado classes banco

--- src/main/java/org/example/database/BancoDeVendas.java
+++ src/main/java/org/example/database/BancoDeVendas.java
@@ -8,18 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BancoDeVendas {
-    BancoDeClientes bancoDeClientes = new BancoDeClientes();
-    BancoDeVendedores bancoDeVendedores = new BancoDeVendedores();
     private List<Venda> listaVendas = new ArrayList<>();
 
     public void cadastrarVenda(Venda venda, Vendedor vendedor, Cliente cliente){
-        if (bancoDeClientes.verificarClienteExiste(cliente.getCpf()) && bancoDeVendedores.verificarVendedorExiste(vendedor.getCpf())) {
-            venda.setCliente(cliente);
-            venda.setVendedor(vendedor);
-            listaVendas.add(venda);
-        } else {
-            throw new IllegalArgumentException("Erro: Vendedor ou Cliente não estão cadastrados no sistema");
-        }
+        venda.setCliente(cliente);
+        venda.setVendedor(vendedor);
+        listaVendas.add(venda);
     }
 
     public List<Venda> getListaVendas() {
