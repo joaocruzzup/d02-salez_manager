a5cd0d92bd84c636d40bfe3e98f73dd158558dc5
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-05 02:35:09 -0300

    :test_tube: test: criado teste para classes bancos

--- src/test/java/BancoDeVendasTest.java
+++ src/test/java/BancoDeVendasTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BancoDeVendasTest {
+import org.example.database.BancoDeClientes;
+import org.example.database.BancoDeVendas;
+import org.example.model.Cliente;
+import org.example.model.Venda;
+import org.example.model.Vendedor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@ExtendWith(MockitoExtension.class)
+public class BancoDeVendasTest {
+    @Mock
+    private BancoDeVendas bancoDeVendas;
+
+    @Test
+    public void cadastrarClienteTest(){
+        Cliente cliente = new Cliente("Joao", "123456789", "joao@example.com");
+        Vendedor vendedor = new Vendedor("Maria", "132456897", "maria@example.com");
+        Venda venda = new Venda(cliente, vendedor, BigDecimal.valueOf(200.0), LocalDate.now());
+
+        Mockito.when(bancoDeVendas.cadastrarVenda(venda, vendedor, cliente)).thenReturn(true);
+        bancoDeVendas.cadastrarVenda(venda, vendedor, cliente);
+        Mockito.verify(bancoDeVendas, Mockito.times(1)).cadastrarVenda(venda, vendedor, cliente);
+    }
 }
